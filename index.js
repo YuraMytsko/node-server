82a374a384ddca47665d2c645e69e2d19c98604c
Yura <yura@gmail.com>
2023-05-18 12:43:11 +0000

    test 5

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get('/', async (req, res) => {
 
     // Construct the response HTML
     const html = `
-      <h1>Page loaded ${requestId} times. test 3</h1>
+      <h1>Page loaded ${requestId} times. test 5</h1>
       <h2>Last requests:</h2>
       <ul>
         ${lastRequests.map(request => `<li>${request.ip} - ${request.userAgent}</li>`).join('')}
